e488da25abcde5ebf6ac85337c4afb4c773edf5d
icyflame <kannan.siddharth12@gmail.com>
2014-09-28 22:34:41 +0530

    Designation of an administrator must be present. Validation added in the model

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,4 +1,5 @@
 class Administrator < ActiveRecord::Base
 	validates :name, presence: true
+	validates :designation, presence: true
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
