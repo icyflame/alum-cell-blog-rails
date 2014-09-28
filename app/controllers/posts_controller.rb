0509111d16eb2dc88acb067a0659ad3949885b3d
icyflame <kannan.siddharth12@gmail.com>
2014-09-28 20:29:17 +0530

    Adding a new post, and wiring up stuff so that the post is connected to its contributor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    @contributor = Contributor.find(params[:contributor_id])
+
+    if @post = @contributor.posts.create(post_params)
+
+      redirect_to post_path(@post)
+
+    else
+
+      render 'new'
+
+    end
+
+    # @post = Post.new(post_params)
+
+    # render plain: params[:post].inspect
+
   end
 
   def new
@@ -12,11 +28,21 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+
+    @contributor = Contributor.find(@post.contributor_id)
+    
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :tags)
   end
 end
