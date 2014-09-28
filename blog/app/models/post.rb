ed9bc75f0d4cb2f5f6e27d52d2eb6677e3b1c050
icyflame <kannan.siddharth12@gmail.com>
2014-09-28 15:22:13 +0530

    Added the validation data for all the three models.

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :contributor
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { minimum: 20 }
 end
