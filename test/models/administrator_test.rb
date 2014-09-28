bca9d7376b76f8cb9935f066dee8bcc487f81bd4
icyflame <kannan.siddharth12@gmail.com>
2014-09-28 17:23:56 +0530

    Added a dummy test in the administrator test module.

--- test/models/administrator_test.rb
+++ test/models/administrator_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class AdministratorTest < ActiveSupport::TestCase
+	test "Sample AdministratorTest" do
+		admin = Administrator.new
+		assert_not admin.save, "Save administrator without the name"
+	end
   # test "the truth" do
   #   assert true
   # end
