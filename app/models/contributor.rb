85ed742fa1441db06fdbb7341a09b8328afb88b5
icyflame <kannan.siddharth12@gmail.com>
2014-09-29 22:50:18 +0530

    Removed email confirmation from both devise models

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,7 +1,7 @@
 class Contributor < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :posts
 	validates :name, presence: true
