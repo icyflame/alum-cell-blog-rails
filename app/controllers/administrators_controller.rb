35ad5c106beb2bb9a0247b881d9b7adfcb24a9c3
icyflame <kannan.siddharth12@gmail.com>
2014-09-30 00:03:58 +0530

    The verification function in the controller.
    Routes.rb has been configured already

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -6,5 +6,20 @@ class AdministratorsController < ApplicationController
   def show
     @administrator = Administrator.find(params[:id])
   end
+
+  def verify_token
+  	# render plain: params.inspect
+  	if params[:verificationstring] == current_administrator.verification_string
+  		@administrator = current_administrator
+  		@administrator.update(verified: true)
+  		flash[:alert] = "You have been validated as an administrator."
+  	else
+  		flash[:alert] = "You entered the wrong verfication token."
+  	end
+  	redirect_to posts_path
+  end
+
+  def verify
+  end
   
 end
