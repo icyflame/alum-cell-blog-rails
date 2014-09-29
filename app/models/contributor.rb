2803819cf05ffa8abc4ee36e3d540220158d9e42
icyflame <kannan.siddharth12@gmail.com>
2014-09-29 22:12:28 +0530

    The deployment branch can have the registration to be confirmable.
    The link sent to the email needs to be clicked and verified in order
    to complete the registration process

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,7 +1,7 @@
 class Contributor < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :posts
 	validates :name, presence: true
